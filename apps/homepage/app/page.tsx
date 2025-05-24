39d5b6752f48d3e2e328931e6ff9b734aee40405
jeffreywescott <jeffrey.wescott@gmail.com>
2025-05-23 20:24:59 -0700

    use Image component

--- apps/homepage/app/page.tsx
+++ apps/homepage/app/page.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center" style={{ backgroundColor: '#101110' }}>
-      <img
+      <Image
         src="/kasho-wordmark.png"
         alt="Kasho Wordmark"
+        width={2400}
+        height={1200}
         className="max-w-2xl h-auto"
+        priority
       />
       <div className="mt-8 flex flex-col items-center">
         <h2 className="text-white text-2xl font-bold font-mono text-center">
