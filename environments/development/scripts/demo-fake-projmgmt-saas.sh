165d4cf0f771d3b3d2937eb6ccb38f770b2a44f7
jeffreywescott <jeffrey.wescott@gmail.com>
2025-05-25 13:23:22 -0700

    merged demo and development environments

---
+++ environments/development/scripts/demo-fake-projmgmt-saas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SQL_DIR="$SCRIPT_DIR/../sql"
+
+# Set up data
+echo "Setting up fake data..."
+cat "$SQL_DIR/fake_projmgmt_saas.sql" | docker exec -i development-postgres-primary-1 psql -U postgres -d source_db
+
+echo "...complete!" 
