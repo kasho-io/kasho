987935f6df5e27a538977c2c815fd7fb6abc2d37
jeffreywescott <jeffrey.wescott@gmail.com>
2025-07-05 23:18:15 +0200

    add information about licensing / design partnerships

--- apps/docs/content/installation/_meta.js
+++ apps/docs/content/installation/_meta.js
@@ -1,6 +1,7 @@
 export default {
   'container-images': 'Container Images',
   'database-setup': 'Database Setup',
+  licensing: 'Licensing',
   configuration: 'Configuration',
   bootstrap: 'Bootstrap Process'
 }
