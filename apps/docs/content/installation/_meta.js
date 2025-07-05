e85e438bb858d35c5950294db3edbe199411bb07
Maya Li <maya.hao.li@gmail.com>
2025-07-05 14:41:16 -0700

    re-run prettier

--- apps/docs/content/installation/_meta.js
+++ apps/docs/content/installation/_meta.js
@@ -1,14 +1,7 @@
 export default {
-<<<<<<< HEAD
-  'container-images': 'Container Images',
-  'database-setup': 'Database Setup',
-  licensing: 'Licensing',
-  configuration: 'Configuration',
-  bootstrap: 'Bootstrap Process'
-}
-=======
+  "container-images": "Container Images",
   "database-setup": "Database Setup",
+  licensing: "Licensing",
   configuration: "Configuration",
   bootstrap: "Bootstrap Process",
 };
->>>>>>> 1f9062b (prettier all the apps)
