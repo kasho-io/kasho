8914d4d5e6fc092e179a9421d9da5f7f1ae99bdc
Maya Li <maya.hao.li@gmail.com>
2025-07-05 11:21:47 -0700

    instrument prettier

--- apps/demo/eslint.config.mjs
+++ apps/demo/eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +10,6 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
 
-export default eslintConfig;
+export default [...eslintConfig, ...eslintConfigPrettier];
