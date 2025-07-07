480d2086663718db35e0d4eaa004a54bc0c0435c
Maya Li <maya.hao.li@gmail.com>
2025-07-06 18:29:58 -0700

    prettier

--- apps/docs/components/ObfuscatedEmail.tsx
+++ apps/docs/components/ObfuscatedEmail.tsx
@@ -1,32 +1,32 @@
-'use client'
+"use client";
 
 interface ObfuscatedEmailProps {
-  user: string
-  domain: string
-  className?: string
+  user: string;
+  domain: string;
+  className?: string;
 }
 
 export function ObfuscatedEmail({ user, domain, className }: ObfuscatedEmailProps) {
-  const email = `${user}@${domain}`
-  const reversedEmail = email.split('').reverse().join('')
-  
+  const email = `${user}@${domain}`;
+  const reversedEmail = email.split("").reverse().join("");
+
   const handleClick = () => {
-    window.location.href = `mailto:${email}`
-  }
-  
+    window.location.href = `mailto:${email}`;
+  };
+
   return (
-    <span 
+    <span
       className={className}
       style={{
-        unicodeBidi: 'bidi-override',
-        direction: 'rtl',
-        textDecoration: 'underline',
-        cursor: 'pointer'
+        unicodeBidi: "bidi-override",
+        direction: "rtl",
+        textDecoration: "underline",
+        cursor: "pointer",
       }}
       onClick={handleClick}
       title="Click to email"
     >
       {reversedEmail}
     </span>
-  )
+  );
 }
